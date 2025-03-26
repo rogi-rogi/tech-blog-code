d63d8dd82af5f24592d27b3f70f4afc4bcbf2538
rogi-rogi <yrkim6883@gmail.com>
2025-03-26 19:46:02 +0900

    Java PriorityQueue 주석 추가

--- pl/java/D250326/PriorityQueueExample.java
+++ pl/java/D250326/PriorityQueueExample.java
@@ -5,14 +5,15 @@ import java.util.PriorityQueue;
 public class PriorityQueueExample {
     public static void main(String[] args) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(20);
-        pq.add(5);
-        pq.add(15);
 
-        System.out.println(pq);         // [5, 20, 15]
-        System.out.println(pq.peek());  // 5
-        pq.poll();
-        System.out.println(pq);         // [15, 20]
-        System.out.println(pq.poll());  // 15, [20]
+        pq.add(20);                     // [20]
+        pq.offer(5);                 // [5, 20]
+        System.out.println(pq.add(15)); // [5, 15, 20], 출력: true
+
+        System.out.println(pq);         // 출력: [5, 20, 15]
+        System.out.println(pq.peek());  // 출력: 5
+        pq.poll();                      // 5 제거
+        System.out.println(pq);         // 출력:[15, 20]
+        System.out.println(pq.poll());  // [20], 출력: 15
     }
 }
